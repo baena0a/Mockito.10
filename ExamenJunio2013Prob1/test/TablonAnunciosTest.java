f2401014ceb8e00132b3953da5c8affb74771550
alejandro baena perez <alejandro baena perez>
2016-05-26 23:49:03 +0200

    añado dos nuevos test

--- ExamenJunio2013Prob1/test/TablonAnunciosTest.java
+++ ExamenJunio2013Prob1/test/TablonAnunciosTest.java
@@ -1,12 +1,36 @@
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.*;
+
+import junio2013.Anuncio;
+import junio2013.IBaseDeDatosDeAnunciantes;
+import junio2013.IBaseDeDatosDePagos;
+import junio2013.TablonDeAnuncios;
 
 public class TablonAnunciosTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+private TablonDeAnuncios listaAnuncio;
+private static final IBaseDeDatosDeAnunciantes baseAnunciante = null;
+private static final IBaseDeDatosDePagos basePagos = null;
+
+	@Before
+	public void SetUp(){
+		listaAnuncio = new TablonDeAnuncios();
 	}
+	@Test
+	public void AnuncioEnTablon() {
+		assertEquals(1, listaAnuncio.anunciosPublicados());
+	}
+	@Test
+	public void publicarAnuncio() {
+		Anuncio anun = new Anuncio("", "", "LA EMPRESA");
+		listaAnuncio.publicarAnuncio(anun, baseAnunciante,basePagos);
+		assertEquals(2, listaAnuncio.anunciosPublicados());
+		
+	}	
+
 
 }
