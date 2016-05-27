977a843b9f509b24acc3d99469dd14442fa6a37a
alejandro baena perez <alejandro baena perez>
2016-05-27 17:50:14 +0200

    Publicados test restantes

---
+++ ExamenJunio2013Prob1/src/junio2013/AnuncianteException.java
@@ -0,0 +1,8 @@
+package junio2013;
+
+public class AnuncianteException extends RuntimeException {
+
+	public AnuncianteException(String mensaje){
+		super(mensaje);
+	}
+}
--- ExamenJunio2013Prob1/test/TablonAnunciosTest.java
+++ ExamenJunio2013Prob1/test/TablonAnunciosTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.when;
 import org.junit.Before;
 import org.junit.Test;
 
+import junio2013.AnuncianteException;
 import junio2013.Anuncio;
 
 import junio2013.IBaseDeDatosDeAnunciantes;
@@ -73,6 +74,48 @@ private TablonDeAnuncios listaAnuncio;
 		assertEquals(anun2, listaAnuncio.buscarAnuncioPorTitulo(anun2.titulo_));
 		assertEquals(3, listaAnuncio.anunciosPublicados());
 		
+	}
+	@Test
+	public void Test6() {
+		Anuncio anun1 = new Anuncio("2",	 ""	, "LA EMPRESA");
+		Anuncio anun2 = new Anuncio("3",	 ""	, "LA EMPRESA");
+		IBaseDeDatosDeAnunciantes baseAnunciante = mock(IBaseDeDatosDeAnunciantes.class);
+		IBaseDeDatosDePagos basePagos = mock(IBaseDeDatosDePagos.class);
+		
+		listaAnuncio.publicarAnuncio(anun1, baseAnunciante, basePagos);
+		listaAnuncio.publicarAnuncio(anun2, baseAnunciante, basePagos);
+		
+		listaAnuncio.borrarAnuncio(anun1.titulo_, anun1.anunciante_);
+		
+		assertEquals(null, listaAnuncio.buscarAnuncioPorTitulo(anun1.titulo_));
+		assertEquals(2, listaAnuncio.anunciosPublicados());
+		
+		
+	}
+	
+	@Test
+	public void Test7() {
+		Anuncio anun1 = new Anuncio("2",	 ""	, "LA EMPRESA");
+		Anuncio anun2 = new Anuncio("3",	 ""	, "LA EMPRESA");
+		IBaseDeDatosDeAnunciantes baseAnunciante = mock(IBaseDeDatosDeAnunciantes.class);
+		IBaseDeDatosDePagos basePagos = mock(IBaseDeDatosDePagos.class);
+		
+		listaAnuncio.publicarAnuncio(anun1, baseAnunciante, basePagos);
+		assertEquals(2, listaAnuncio.anunciosPublicados());
+		listaAnuncio.publicarAnuncio(anun2, baseAnunciante, basePagos);
+		assertEquals(2, listaAnuncio.anunciosPublicados());
+	}
+	
+	@Test(expected = AnuncianteException.class)
+	public void Test8(){
+		Anuncio anun1 = new Anuncio("anuncio 2", "segundo anuncio", "OTRA EMPRESA");
+		IBaseDeDatosDeAnunciantes bdAnunciantes = mock(IBaseDeDatosDeAnunciantes.class);
+		IBaseDeDatosDePagos bdPagos = mock(IBaseDeDatosDePagos.class);
+		
+		when(bdAnunciantes.buscarAnunciante(anun1.anunciante_)).thenReturn(false);
+		when(bdPagos.anuncianteTieneSaldo(anun1.anunciante_)).thenReturn(false);
+		
+		listaAnuncio.publicarAnuncio(anun1, bdAnunciantes, bdPagos);
 	}
 
 
